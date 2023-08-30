4d99f1ba4345835f72b78318b87b35df5a4e8e95
Suman Raj Pathak <viper.suman@gmail.com>
2023-08-30 13:29:01 +0545

    fix:Example update

--- example/ip_address_example.dart
+++ example/ip_address_example.dart
@@ -3,5 +3,12 @@ import 'package:public_ip_address/public_ip_address.dart';
 void main() async {
   var ip = await IpAddress.getIp();
   print(ip);
-  // Sample Output : 208.67.222.222
+  // Sample Output : 208.XX.XX.XX
+  var geoIP = await IpAddress.getAllData();
+  print(geoIP);
+  //Sample Output :
+  // {ip: 202.XX.XX.XX, continentCode: AS, country: [COUNTRY NAME],
+  // countryCode: [COUNTRY CODE], countryCode3: [COUNTRY CODE 3],
+  // latitude: [LAT],longitude: [LNG], timeZone: Asia/Kathmandu, offset: 20700,
+  // asn: 4007,organization: [ISP ORGANIZATION NAME] region: [REGION], regionCode: [REGION CODE]}
 }
